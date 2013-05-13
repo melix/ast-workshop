e63ea02badba3a83a3de78d584dff88054ab2144
Cedric Champeau <cedric.champeau@gmail.com>
2013-05-13 19:09:19 +0200

    Solution to exercise 1

--- src/main/java/org/gr8conf/AuthorAdderASTTransformation.java
+++ src/main/java/org/gr8conf/AuthorAdderASTTransformation.java
@@ -15,5 +15,29 @@
  */
 package org.gr8conf;
 
-public class AuthorAdderASTTransformation {
+import org.codehaus.groovy.ast.ASTNode;
+import org.codehaus.groovy.ast.ClassHelper;
+import org.codehaus.groovy.ast.ClassNode;
+import org.codehaus.groovy.ast.expr.ConstantExpression;
+import org.codehaus.groovy.control.CompilePhase;
+import org.codehaus.groovy.control.SourceUnit;
+import org.codehaus.groovy.transform.AbstractASTTransformation;
+import org.codehaus.groovy.transform.GroovyASTTransformation;
+
+import java.util.List;
+
+@GroovyASTTransformation(phase= CompilePhase.CONVERSION)
+public class AuthorAdderASTTransformation extends AbstractASTTransformation {
+    @Override
+    public void visit(final ASTNode[] nodes, final SourceUnit source) {
+        List<ClassNode> classes = source.getAST().getClasses();
+        for (ClassNode node: classes) {
+            node.addField(
+                    "$AUTHOR",
+                    ACC_PUBLIC | ACC_STATIC | ACC_FINAL,
+                    ClassHelper.STRING_TYPE,
+                    new ConstantExpression("Cédric Champeau")
+            );
+        }
+    }
 }
