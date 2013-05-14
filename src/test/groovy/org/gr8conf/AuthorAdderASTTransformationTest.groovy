a52044d122902f90d457a2c3107f5bf6df4e8e1f
Cedric Champeau <cedric.champeau@gmail.com>
2013-05-14 11:04:44 +0200

    Fix assertions in @ASTTest

--- src/test/groovy/org/gr8conf/AuthorAdderASTTransformationTest.groovy
+++ src/test/groovy/org/gr8conf/AuthorAdderASTTransformationTest.groovy
@@ -6,11 +6,22 @@ class AuthorAdderASTTransformationTest extends GroovyTestCase {
         assertScript '''import org.gr8conf.Author
             import groovy.transform.ASTTest
             import org.codehaus.groovy.ast.*
+            import org.codehaus.groovy.ast.expr.ConstantExpression
+            import static org.codehaus.groovy.ast.ClassHelper.*
             import static org.codehaus.groovy.control.CompilePhase.*
 
             @Author('John Doe')
             @ASTTest(phase=SEMANTIC_ANALYSIS, value={
-                assert classNode instanceof ClassNode
+                assert node instanceof ClassNode
+                def fn = node.getDeclaredField('$AUTHOR')
+                assert fn instanceof FieldNode
+                assert fn.type == STRING_TYPE
+                assert fn.static
+                assert fn.public
+                assert fn.final
+                def initialExpr = fn.initialExpression
+                assert initialExpr instanceof ConstantExpression
+                assert initialExpr.text == 'John Doe'
             })
             class Foo {
             }
