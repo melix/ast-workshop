149d01fb51d22990fc42aaeabc9220f725e43d94
Cedric Champeau <cedric.champeau@gmail.com>
2013-05-14 10:57:55 +0200

    Introduce @ASTTest

--- src/test/groovy/org/gr8conf/AuthorAdderASTTransformationTest.groovy
+++ src/test/groovy/org/gr8conf/AuthorAdderASTTransformationTest.groovy
@@ -2,9 +2,16 @@ package org.gr8conf
 
 class AuthorAdderASTTransformationTest extends GroovyTestCase {
 
-    void testASTTransformationShouldBeDebuggableFromIDE() {
+    void testUsingASTTest() {
         assertScript '''import org.gr8conf.Author
+            import groovy.transform.ASTTest
+            import org.codehaus.groovy.ast.*
+            import static org.codehaus.groovy.control.CompilePhase.*
+
             @Author('John Doe')
+            @ASTTest(phase=SEMANTIC_ANALYSIS, value={
+                assert classNode instanceof ClassNode
+            })
             class Foo {
             }
             assert Foo.$AUTHOR == 'John Doe'
